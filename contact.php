608c033eac504fb25354049a02bbe0f4c89ec136
RAJNISH KUMAR <50701273+rajnishkumar13500@users.noreply.github.com>
2021-05-23 07:12:32 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,16 +3,24 @@
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
-		$from = 'Parax Contact Form'; 
-		$to = 'rajnishkumar13500@gmail.com'; 
+		$fromName = 'RAJNISH KUMAR'; 
+		$fromEmail = 'rajnishkumar13500@gmail.com'; 
 		$subject = $_POST['subject']; 
 		
 		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
 	
 
-	mail($to, $subject, $body, $from) or die("Error!");
+		$send = SimpleMail::make()
+    		->setTo($email, $name)
+    		->setFrom($fromEmail, $fromName)
+    		->setSubject($subject)
+   		 ->setMessage($message)
+   		 ->setReplyTo($replyEmail, $replyName)
+   		 ->setHtml("thank-you.html")
+   		 ->setWrap(100)
+   		 ->send();
 
-	header("location: thank-you.html");
+	
 	
 	}
 	
